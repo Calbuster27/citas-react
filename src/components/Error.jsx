0f057769e6eadbaa611a62a0560273d70b92b943
Jose Rodriguez <joselan27@outlook.com>
2023-09-10 13:58:46 -0400

    first commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,13 @@
+
+
+const Error = () => {
+  return (
+    <div className="bg-red-800 font-bold text-center rounded mb-5 p-3 text-lg uppercase text-white">
+            <p>
+             Todos los campos son obligatorios
+            </p>
+    </div>
+  )
+}
+
+export default Error
